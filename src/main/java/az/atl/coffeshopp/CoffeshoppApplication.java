836eff643bacf8ac35ef1e693b62aab091829ff7
Ali <ibrahimzade.ali99@gmail.com>
2024-08-04 23:51:24 +0400

    Initial commit

---
+++ src/main/java/az/atl/coffeshopp/CoffeshoppApplication.java
@@ -0,0 +1,13 @@
+package az.atl.coffeshopp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeshoppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeshoppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/az/atl/coffeshopp/CoffeshoppApplicationTests.java
@@ -0,0 +1,13 @@
+package az.atl.coffeshopp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeshoppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
